9e2622f924f4eff92b7fbd1be851e30967fe0e19
agustinjulian <ajulian@fi.uba.ar>
2025-12-15 23:57:43 -0300

    Fixes

--- src/controllers/controladores.js
+++ src/controllers/controladores.js
@@ -7,7 +7,7 @@ export const agregarProducto = async (req, res) => {
         const nuevoProducto = await servicios.agregarProducto(producto)
         res.status(200).json(nuevoProducto)
     }catch(error){
-        res.status(500)
+        res.status(500).json({message: "Error"})
     }
 }
 
@@ -15,24 +15,26 @@ export const eliminarProducto = async (req, res) => {
     try{
         const id = req.params.id
         if(!id)
-            res.status(400)
+            res.status(400).json({message: "Error"})
 
         await servicios.eliminarProducto(id)
-        res.status(200)
+        res.status(200).json({message: "producto eliminado"})
     }catch(error){
-        res.status(500)
+        res.status(500).json({message: "Error"})
     }
 }
 
 export const editarProducto = async (req, res) => {
     try{
         const id = req.params.id
-        if(!id || !producto)
-            res.status(400)
         const producto = req.body
+        if(!id || !producto)
+            res.status(400).json({message: "Error"})
+        
         const nuevoProducto = await servicios.editarProducto(id, producto)
+        res.status(200).json(nuevoProducto)
     }catch(error){
-        res.status(500)
+        res.status(500).json({message: "Error"})
     }
 }
 
@@ -41,7 +43,7 @@ export const obtenerProductos = async (req, res) => {
         const productos = await servicios.obtenerProductos()
         res.status(200).json(productos)
     }catch(error){
-        res.status(500)
+        res.status(500).json({message: "Error"})
     }
     
 }
@@ -58,7 +60,7 @@ export const obtenerProductoPorId = async (req, res) => {
         else
             res.status(404).json({message: "Producto no encontrado"})
     }catch(error){
-        res.status(500)
+        res.status(500).json({message: "Error"})
     }
     
 }
