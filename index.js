ea7592ad57ce69f481ac75a7fca45e7b3d2dbd3c
agustinjulian <ajulian@fi.uba.ar>
2025-12-16 01:56:19 -0300

    Ajustes a mensajes de solicitudes

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(authentication)
 app.use('/api', rutas)
 
 app.use((req, res, next)=>{
-    res.status(404).send(`ruta invalida pa`)
+    res.status(404).send(`ruta invalida`)
 })
 
 app.listen(PORT, () => {
